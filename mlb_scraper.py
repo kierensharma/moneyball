3a73c5b0549bc69571317f8133ab467ca3821c10
kierensharma <kieren.sharma@gmail.com>
2020-06-19 16:53:29 +0100

    Created empty DataFrame ready to fill with player data.

--- mlb_scraper.py
+++ mlb_scraper.py
@@ -2,3 +2,18 @@ import pandas as pd
 import re
 import requests
 from bs4 import BeautifulSoup
+
+# Extracting HTML source code from ESPN website.
+url = "http://www.espn.com/mlb/history/leaders/_/breakdown/season/year/2019"
+
+# Create a BeautifulSoup of the source code and parse in HTML.
+page = requests.get(url)
+soup = BeautifulSoup(page.text, 'html.parser')
+
+# Extract the column names from the soup.
+header = soup.find('tr', attrs={'class':'colhead'})
+columns = [col.get_text() for col in header.find_all('td')]
+
+# Create a DataFrame with column names from above
+player_data = pd.DataFrame(columns=columns)
+print(player_data)
