70261bb575790082da61c8fb8a3cc55fcfbef2f9
kierensharma <kieren.sharma@gmail.com>
2020-06-19 17:07:34 +0100

    Created for loop to scrape player data and concatenate with DataFrame.

--- mlb_scraper.py
+++ mlb_scraper.py
@@ -14,6 +14,18 @@ soup = BeautifulSoup(page.text, 'html.parser')
 header = soup.find('tr', attrs={'class':'colhead'})
 columns = [col.get_text() for col in header.find_all('td')]
 
-# Create a DataFrame with column names from above
-player_data = pd.DataFrame(columns=columns)
-print(player_data)
+# Create a DataFrame with column names from above.
+player_df = pd.DataFrame(columns=columns)
+
+# Gather the player data from each row of the ESPN table.
+players = soup.find_all('tr', attrs={'class':re.compile('row player-10-')})
+for player in players:
+    # Get the stats for each player.
+    stats = [stats.get_text() for stat in player.find_all('td')]
+
+    # Create a temp DataFrame for current player's stats.
+    temp_df = pd.DataFrame(stats).transpose()
+    temp_df.columns = columns
+
+    # Concatenate temp_df with player_df
+    player_df = pd.concat([player_df,temp_df], ignore_index=True)
